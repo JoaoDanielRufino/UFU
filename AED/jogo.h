b216f714b498fad30643b408b8854798eb01dcc2
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-07-18 10:51:14 -0300

    Create jogo.h

---
+++ AED/jogo.h
@@ -0,0 +1,14 @@
+#ifndef JOGO_H
+#define JOGO_H
+#include "queue.h"
+#include "list.h"
+
+Player* jogo(List **li, Queue* q, int comeco, int fim);
+
+int random(List* li, Player* pl, Node* aux);
+
+int not_visited_path(List* li, Player* pl);
+
+void backtrack(Player* pl);
+
+#endif
