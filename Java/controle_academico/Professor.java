becc3c4657ebb2b86c800a3ca477e111eb97cb6a
JoaoDanielRufino <joaodaniel0405@gmail.com>
2017-09-27 21:20:07 -0300

    Create Professor.java

---
+++ Java/controle_academico/Professor.java
@@ -0,0 +1,12 @@
+package teste;
+
+public class Professor {
+    String nome;
+    String materia;
+    
+    public Professor (String nome, String materia){
+        this.nome = nome;
+        this.materia = materia;
+    }
+    
+}
