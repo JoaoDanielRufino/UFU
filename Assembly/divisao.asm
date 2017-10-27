ddbb4c3cc132bc34a1b644b02c520b0c7d4eadca
João Daniel Rufino <joaodaniel0405@gmail.com>
2017-10-27 16:39:28 -0200

    Create divisao.asm

---
+++ Assembly/divisao.asm
@@ -0,0 +1,44 @@
+.data
+	Num1: .asciiz "Digite o primeiro numero: "
+	Num2: .asciiz "Digite o segundo numero: "
+	Result1: .asciiz "Resultado da parte inteira: "
+	Result2: .asciiz "\nResto da divisao: "
+
+.text
+	li $v0,4 #Impressao de string.
+	la $a0,Num1 #Mostra a string de Num1.
+	syscall
+	
+	li $v0,5 #Le um intreiro.
+	syscall
+	
+	move $t0,$v0 #Move $v0 para $t0, para armazenar o valor lido.
+	
+	li $v0,4 #Impressao de string.
+	la $a0,Num2 #Mostra a string de Num2.
+	syscall
+	
+	li $v0,5 #Le um intreiro.
+	syscall
+	
+	move $t1,$v0 #Move $v0 para $t1, para armazenar o valor lido.
+	
+	div $t2,$t0,$t1 #Divide o valor de $t0 com $t1.
+	
+	li $v0,4 #Impressao de string.
+	la $a0,Result1 #Mostra a string de Result.
+	syscall
+	
+	li $v0,1 #Impressao de inteiros.
+	move $a0,$t2 #Move $s3 para $a0 para printar o resultado.
+	syscall
+	
+	mfhi $t6 #Modulo da divisao entre $t0 e $t1
+	
+	li $v0,4 #Impressao de string.
+	la $a0,Result2 #Mostra a string de Result.
+	syscall
+	
+	li $v0,1 #Impressao de inteiros.
+	move $a0,$t6 #Move $s3 para $a0 para printar o resultado.
+	syscall
