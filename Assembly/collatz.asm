ffb268d028b797b6bf1d66baee73b285d3396bcc
João Daniel Rufino <joaodaniel0405@gmail.com>
2017-11-24 19:12:35 -0200

    Update collatz.asm

--- Assembly/collatz.asm
+++ Assembly/collatz.asm
@@ -3,22 +3,28 @@
 
 .data
 	Frase: .asciiz "Digite um numero: "
+	Erro: .asciiz "Erro, digite um numero maior que 0!!\n"
 	Espaco: .asciiz " "
 	
 .text
 
-	li $t1, 2 #Valores para uso no decorrer do programa.
-	li $t2, 3
-	li $t3, 1
-
+leNatural:
 	li $v0, 4 
 	la $a0, Frase #Imprime a frase.
 	syscall
-	
+			
 	li $v0, 5 #Le um inteiro.
 	syscall
 	
 	move $t0, $v0 #Armazena o conteudo de $v0 em $t0.
+		
+	bgt $t0, 0, loop
+	
+	li $v0, 4 
+	la $a0, Erro #Imprime a frase.
+	syscall
+	
+	j leNatural
 	
 loop:
 	li $v0, 1
@@ -30,7 +36,7 @@ loop:
 	syscall
 	
 	jal verificaNumero
-	bgt $t0, $t3, loop #$t0 < $t3 ? sai do loop : loop
+	bgt $t0, 1, loop #$t0 < $t3 ? sai do loop : loop
 	
 	li $v0, 1
 	move $a0, $t0 #Impressao de inteiro.
@@ -40,16 +46,17 @@ loop:
 	syscall #Exit.
 	
 verificaNumero:
+	li $t1, 2
 	div $t0, $t1 
 	mfhi $t4 #Guarda o resto da divisao por 2.
 	beqz $t4, numPar #$t4 == 0 ? numPar : numImpar
 	j numImpar
 	
 numPar:
-	div $t0, $t0, $t1 #Armazena a divisao $t0/$t1.
+	div $t0, $t0, 2 #Armazena a divisao $t0/$t1.
 	jr $ra #Retorna.
 	
 numImpar:
-	mul $t0, $t0, $t2 #Armazena a multiplicacao $t0 * $t1.
+	mul $t0, $t0, 3 #Armazena a multiplicacao $t0 * $t1.
 	addi $t0, $t0, 1 #Armazena a soma $t0 + 1.
 	jr $ra #Retorna.
