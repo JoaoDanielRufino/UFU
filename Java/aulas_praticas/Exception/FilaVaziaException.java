2664a6e0df571b68ceff990f301cc8ddffdc867b
João Daniel Rufino <joaodaniel0405@gmail.com>
2017-12-06 18:52:52 -0200

    Create FilaVaziaException.java

---
+++ Java/aulas_praticas/Exception/FilaVaziaException.java
@@ -0,0 +1,8 @@
+package testefila;
+
+public class FilaVaziaException extends ArrayIndexOutOfBoundsException {
+    
+    public FilaVaziaException (String message){
+        super(message);
+    }
+}
