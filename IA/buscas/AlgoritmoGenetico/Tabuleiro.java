b179f4e289aa5dadeae3145c07e2f37b1876fe1e
João Daniel Rufino <joaodaniel0405@gmail.com>
2019-04-09 23:09:51 -0300

    Update Tabuleiro.java

--- IA/buscas/AlgoritmoGenetico/Tabuleiro.java
+++ IA/buscas/AlgoritmoGenetico/Tabuleiro.java
@@ -1,19 +1,33 @@
 package algoritmogenetico;
 
-public class AlgoritmoGenetico {
-    public static final int DAMAS_QTD = 8;
-    public static final int POP_QTD = 5;
+public class Tabuleiro {
+    private DNA dna;
+    private int fitness;
     
-    public static void main(String[] args) {
-        Populacao p = new Populacao();
-        
-        for(int i = 0; i < 30; i++) {
-            p.calculaFitness();
-            p.printaMelhor();
-            p.selecaoNatural();
-            p.novaGeracao();
+    public Tabuleiro(DNA dna) {
+        this.dna = dna;
+        this.fitness = 0;
+    }
+    
+    public void calculaFitness() {
+        int size = AlgoritmoGenetico.DAMAS_QTD;
+        for(int i = 0; i < size-1; i++) {    
+            int aux = 1;
+            for(int j = i+1; j < size; j++) {
+                if(this.dna.geneAt(i) != this.dna.geneAt(j) && this.dna.geneAt(j) != this.dna.geneAt(i)+aux && this.dna.geneAt(j) != this.dna.geneAt(i)-aux) {
+                    this.fitness++;
+                }
+                aux++;
+            }         
         }
-        
+    }
+    
+    public DNA getDNA() {
+        return this.dna;
+    }
+    
+    public int getFitness() {
+        return this.fitness;
     }
     
 }
