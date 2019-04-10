5328436fe447365531853391e91b5f179765aadf
João Daniel Rufino <joaodaniel0405@gmail.com>
2019-04-09 23:10:11 -0300

    Update AlgoritmoGenetico.java

--- IA/buscas/AlgoritmoGenetico/AlgoritmoGenetico.java
+++ IA/buscas/AlgoritmoGenetico/AlgoritmoGenetico.java
@@ -2,18 +2,18 @@ package algoritmogenetico;
 
 public class AlgoritmoGenetico {
     public static final int DAMAS_QTD = 8;
-    public static final int POP_QTD = 5;
+    public static final int POP_QTD = 40;
     
     public static void main(String[] args) {
         Populacao p = new Populacao();
         
-        for(int i = 0; i < 30; i++) {
+        while(!p.getEnd()) {
             p.calculaFitness();
             p.printaMelhor();
             p.selecaoNatural();
             p.novaGeracao();
         }
         
-    }
+    }   
     
 }
