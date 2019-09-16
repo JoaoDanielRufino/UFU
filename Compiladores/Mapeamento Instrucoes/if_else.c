65c11d111012bb200d236f0b66d4d189f58e1fd8
João Daniel Rufino <joaodaniel0405@gmail.com>
2019-09-16 15:38:56 -0300

    Add files via upload

---
+++ Compiladores/Mapeamento Instrucoes/if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+  int i = 10;
+
+  if(i > 5)
+    i--;
+  else
+    i++;
+
+  return 0;
+}
---
+++ Compiladores/Mapeamento Instrucoes/int.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+
+  int x = 10;
+
+  return 0;
+}
