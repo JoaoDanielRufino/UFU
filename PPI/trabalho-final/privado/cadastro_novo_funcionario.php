24a3cafed2074a131a6f91d8034520e59b0fbb13
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-12-05 16:47:53 -0300

    cadastro funcionario
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- PPI/trabalho-final/privado/cadastro_novo_funcionario.php
+++ PPI/trabalho-final/privado/cadastro_novo_funcionario.php
@@ -11,23 +11,47 @@
   $estado = $_POST["estado"];
   $data_inicio = $_POST["data_inicio"];
   $salario = $_POST["salario"];
-  $senha = $_POST["senha"];
+  $senha =  password_hash($_POST["senha"], PASSWORD_DEFAULT);;
   $especialidade = isset($_POST["especialidade"]) ? $_POST["especialidade"] : "";
   $crm = isset($_POST["crm"]) ? $_POST["crm"] : "";
 
   try {
-    $sql = <<<SQL
-    INSERT INTO novo_funcionario (nome, email, telefone, cep, logradouro, bairro, cidade, estado, 
-    data_inicio, salario, senha, especialidade, crm)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    $sql_pessoa = <<<SQL
+    INSERT INTO pessoa (nome, email, telefone, cep, logradouro, bairro, cidade, estado)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
     SQL;
 
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$nome, $email, $telefone, $cep, $logradouro, $bairro, $cidade, $estado, 
-      $data_inicio, $salario, $senha, $especialidade, $crm]);
+    $sql_funcionario = <<<SQL
+    INSERT INTO funcionario (data_contrato, salario, senha_hash, codigo)
+    VALUES (?, ?, ?, ?)
+    SQL;
+
+    $sql_medico = NULL;
+    if($especialidade != "") {
+      $sql_medico = <<<SQL
+      INSERT INTO medico (especialidade, crm, codigo)
+      VALUES (?, ?, ?)
+      SQL;
+    }
+
+    $pdo->beginTransaction();
+
+    $stmt_pessoa = $pdo->prepare($sql_pessoa);
+    $stmt_pessoa->execute([$nome, $email, $telefone, $cep, $logradouro, $bairro, $cidade, $estado]);
+
+    $ultimoIdInserido = $pdo->lastInsertId();
+    $stmt_funcionario = $pdo->prepare($sql_funcionario);
+    $stmt_funcionario->execute([$data_inicio, $salario, $senha, $ultimoIdInserido]);
     
+    if($sql_medico != NULL) {
+      $stmt_medico = $pdo->prepare($sql_medico);
+      $stmt_medico->execute([$especialidade, $crm, $ultimoIdInserido]);
+    }
+
+    $pdo->commit();
     echo "Cadastro do funcionário realizado com sucesso!";
   } catch (Exception $e) {
+    $pdo->rollBack();
     exit('Falha ao cadastrar os dados: ' . $e->getMessage());
   }
 ?>
