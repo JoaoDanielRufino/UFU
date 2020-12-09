5a04a80b7b1443a20f51eff67e89cbe38ac5cfa0
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-12-09 16:03:34 -0300

    trabalho6 finalizado
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- PPI/trabalho6/exercicio3/index.php
+++ PPI/trabalho6/exercicio3/index.php
@@ -23,7 +23,7 @@ catch (Exception $e) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tabelas</title>
+  <title>Exercicio 3</title>
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-CuOF+2SnTUfTwSZjCXf01h7uYhfOBuxIhGKPbfEJ3+FqH/s6cIFN9bGr1HmAg4fQ" crossorigin="anonymous">
---
+++ PPI/trabalho6/exercicio4/cadastro-paciente.php
@@ -0,0 +1,48 @@
+<?php
+  require "conexaoMysql.php";
+  $pdo = mysqlConnect();
+
+  $nome = $_POST["nome"];
+  $email = $_POST["email"];
+  $telefone = $_POST["telefone"];
+  $cep = $_POST["cep"];
+  $logradouro = $_POST["logradouro"];
+  $bairro = $_POST["bairro"];
+  $cidade = $_POST["cidade"];
+  $estado = $_POST["estado"];
+  $peso = $_POST["peso"];
+  $altura = $_POST["altura"];
+  $sangue = $_POST["sangue"];
+
+  try {
+    $sql_pessoa = <<<SQL
+    INSERT INTO pessoa (nome, email, telefone, cep, logradouro, bairro, cidade, estado)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    SQL;
+    
+    $sql_paciente = <<<SQL
+    INSERT INTO paciente (peso, altura, tipo_sanguineo, codigo)
+    VALUES (?, ?, ?, ?)
+    SQL;
+
+    $pdo->beginTransaction();
+
+    $stmt_pessoa = $pdo->prepare($sql_pessoa);
+    if(!$stmt_pessoa->execute([$nome, $email, $telefone, $cep, $logradouro, $bairro, $cidade, $estado])) {
+      throw new Exception('Falha ao cadastrar pessoa');
+    }
+
+    $ultimoIdInserido = $pdo->lastInsertId();
+    $stmt_paciente = $pdo->prepare($sql_paciente);
+    if(!$stmt_paciente->execute([$peso, $altura, $sangue, $ultimoIdInserido])) {
+      throw new Exception('Falha ao cadastrar paciente');
+    }
+    
+    $pdo->commit();
+    header("Location: index.html");
+    exit();
+  } catch (Exception $e) {
+    $pdo->rollBack();
+    exit('Falha ao cadastrar os dados: ' . $e->getMessage());
+  }
+?>
