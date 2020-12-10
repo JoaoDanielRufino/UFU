9b0040364f2312a2cdaa51aab68e1935ea0424c0
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-12-09 21:51:13 -0300

    preventDefault nos forms e login pronto
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- PPI/trabalho-final/publico/login.php
+++ PPI/trabalho-final/publico/login.php
@@ -1,40 +1,38 @@
 <?php
-
-function checkLogin($pdo, $email, $senha) {
-  $sql = <<<SQL
-    SELECT f.senha_hash
-    FROM pessoa as p, funcionario as f
-    WHERE p.codigo = f.codigo AND p.email = ?
-    SQL;
-
-  try {
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([$email]);
-    $row = $stmt->fetch();
-    if (!$row)
-      return false;
-    else
-      return password_verify($senha, $row['senha_hash']);
-  } 
-  catch (Exception $e) {
-    //error_log($e->getMessage(), 3, 'log.php');
-    exit('Falha inesperada: ' . $e->getMessage());
-  }
-}
-
   include "../db/db_connection.php";
-
+  
   $email = $senha = "";
 
   if (isset($_POST["email"]))
     $email = $_POST["email"];
   if (isset($_POST["senha"]))
     $senha = $_POST["senha"];
+    
+  try {
+    $sql = <<<SQL
+      SELECT f.senha_hash
+      FROM pessoa as p INNER JOIN funcionario as f
+      ON p.codigo = f.codigo 
+      WHERE p.email = ?
+      SQL;
+    
+      
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$email]);
+    $senha_hash = $stmt->fetch();
 
-  if (checkLogin($pdo, $email, $senha)) {
-    echo "Joooj";
-  } else {
-    echo "Dados incorretos";
+    $obj = new stdClass();
+    if(password_verify($senha, $senha_hash['senha_hash'])) {
+      $obj->response = true;
+    } else {
+      $obj->response = false;
+    }
+  
+    echo json_encode($obj);
+  } 
+  catch (Exception $e) {
+    //error_log($e->getMessage(), 3, 'log.php');
+    exit('Falha inesperada: ' . $e->getMessage());
   }
 
 ?>
