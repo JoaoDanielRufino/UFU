060ba48579de503613f9a268b60294c0c815d669
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-12-14 14:33:03 -0300

    tirando px das tag img
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- PPI/trabalho-final/privado/pacientes.php
+++ PPI/trabalho-final/privado/pacientes.php
@@ -47,7 +47,7 @@
 </head>
 <body>
   <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm">
-    <img src="../images/logo.png" alt="logo" width="50px">
+    <img src="../images/logo.png" alt="logo" width="50">
     <p class="h5 my-0 mr-md-auto fw-normal">Clínica Lawrence</p>
     <nav class="my-2 my-md-0 mr-md-3">
       <a class="p-2 text-dark" href="novo_funcionario.php">Novo Funcionário</a>
